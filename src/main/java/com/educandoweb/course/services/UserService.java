2ddd48ddfbbebce5fb8a0c220c11cc78babaca35
Renato Silveira de Moura <renato@proscore.com.br>
2022-07-18 16:07:46 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,7 +1,10 @@
 package com.educandoweb.course.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -44,10 +47,16 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		Optional<User> objFind = repository.findById(id);
-		User entity = objFind.get();
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			Optional<User> objFind = repository.findById(id);
+			User entity = objFind.get();
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (NoSuchElementException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
